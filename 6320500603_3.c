e9c2661a399b3d8b1ae23e561117912c7f5c76f5
icephat <phattaraporn.sa@ku.th>
2021-03-04 16:04:38 +0700

    เพิ่มการวนลูปเพื่อเช็คตัวอักษร

--- 6320500603_3.c
+++ 6320500603_3.c
@@ -5,6 +5,13 @@ int main()
     int i;
     char a[200]={};
     gets(a);
+    for(i=0;i<=strlen(a);i++)
+    {
+        if(a[i]!=a[i+1])
+        {
+            printf("%c",a[i+1]);
+        }
+    }
 
 
 
