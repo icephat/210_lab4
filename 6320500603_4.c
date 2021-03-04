77759321a13606e049da929c3cee1d9d30bb5ee9
icephat <phattaraporn.sa@ku.th>
2021-03-04 17:19:29 +0700

    แก้ไขโค้ด

--- 6320500603_4.c
+++ 6320500603_4.c
@@ -5,23 +5,24 @@ int main()
 {
     char a[1000],b[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
     char *s;
-    int i=0,j=0,k=0;
-    scanf("%[^\n]s",s);
-    s=strtok(s," ");
+    int i=0,j,k=0;
+    scanf("%[^\n]s",a);
+    s=strtok(a," ");
     while(s!=NULL)
     {
-        for(;j<11;j++)
+        for(j=0;j<11;j++)
         {
             if(strcmp(s,b[j]))
             {
                 k++;
             }
         }
+        if(k==11||i==0)
+        {
+            printf("%c",toupper(s[0]));
+        }
+        s=strtok(NULL," ");
+        i++,k=0;
     }
-
-
-
-
-
     return 0;
 }
