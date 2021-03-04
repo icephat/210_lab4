7129d6b30ac935b0cd1418be7a2153c0b6993942
icephat <phattaraporn.sa@ku.th>
2021-03-04 17:38:33 +0700

    เพิ่มเงื่อนไข

--- 6320500603_5.c
+++ 6320500603_5.c
@@ -2,11 +2,27 @@
 int main()
 {
     int i,j,n,a[4],x,y;
+    char s[100];
     scanf("%d",&n);
     a[0]=1;
-
-
-
+    for(i=0;i<n;i++)
+    {
+        a[0]=1;
+        scanf("%s",s);
+        for(j=0;j<strlen(s);j++)
+        {
+            if(s[j]=='A')
+            {
+                x=a[0];
+                a[0]=a[3];
+                a[3]=x;
+            }
+            else if(s[j]=='B')
+            {
+                x = a[0];
+                a[0] = a[3];
+                a[3] = x;
+            }
 
     return 0;
 }
