fd1641880bcc054054043d0e739ea173b83fd805
icephat <phattaraporn.sa@ku.th>
2021-03-04 14:54:03 +0700

    สร้างการรับค่า
    วนลูปการรับค่าตามโจทย์

---
+++ 6320500603_1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,j,sum;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            scanf("%d",&a[i]);
+        }
+    }
+
+
+
+
+    return 0;
+}
