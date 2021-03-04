77a6c83d8509a39ad3fb9efe5b1a99a199879e5e
icephat <phattaraporn.sa@ku.th>
2021-03-04 15:48:05 +0700

    แก้ไขตรงอาเรย์

--- 6320500603_1.c
+++ 6320500603_1.c
@@ -3,12 +3,12 @@ int main()
 {
     int i,n,j,sum;
     scanf("%d",&n);
-    int a[n];
+    int a[n][3];
     for(i=0;i<n;i++)
     {
         for(j=0;j<3;j++)
         {
-            scanf("%d",&a[i]);
+            scanf("%d",&a[i][j]);
         }
     }
 
