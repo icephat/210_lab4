ba652855fbf8ff0d3dbe783a6342227d2a4adc8f
icephat <phattaraporn.sa@ku.th>
2021-03-04 17:53:19 +0700

    แก้ไขโค้ดเพิ่ม

--- 6320500603_5.c
+++ 6320500603_5.c
@@ -1,21 +1,23 @@
 #include<stdio.h>
+#include<string.h>
 int main()
 {
-    int i,j,n,a[4],x,y;
+    int i,j,n=0,a[4],x,y;
     char s[100];
     scanf("%d",&n);
     a[0]=1;
     for(i=0;i<n;i++)
     {
         a[0]=1;
-        scanf("%s",&s);
+
+        scanf("%s",s);
         for(j=0;j<strlen(s);j++)
         {
             if(s[j]=='A')
             {
-                x=a[0];
-                a[0]=a[3];
-                a[3]=x;
+                x=a[1];
+                a[1]=a[2];
+                a[2]=x;
             }
             else if(s[j]=='B')
             {
@@ -51,7 +53,15 @@ int main()
                 a[1] = y;
             }
         }
-
+         for(j=0;j<4;j++)
+            {
+                if(a[j]==1)
+                {
+                    printf("%d\n",j+1);
+                }
+                a[j] = 0;
+            }
+    }
 
     return 0;
 }
